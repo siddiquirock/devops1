7c3d0834e41716b3e6d78a640eee566730181a0e
adil9015 <aamilsiddiquivgi@gmail.com>
2025-07-12 20:27:53 +0530

    Feature: add conflict line

--- Downloads/vscode/1223/Modules/azurerm_public_ip/main.tf
+++ Downloads/vscode/1223/Modules/azurerm_public_ip/main.tf
@@ -4,3 +4,10 @@ resource "azurerm_public_ip" "pip" {
   location            = var.location
   allocation_method   = var.allocation_method
 }
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.public_ip_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = var.allocation_method
+}
