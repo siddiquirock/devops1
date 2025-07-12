be571f8b11d26286c26e3848fc1b96000d92ee13
adil9015 <aamilsiddiquivgi@gmail.com>
2025-07-12 20:52:13 +0530

    Feature:

--- Downloads/vscode/1223/Modules/azurerm_public_ip/variables.tf
+++ Downloads/vscode/1223/Modules/azurerm_public_ip/variables.tf
@@ -1,11 +1,11 @@
 variable "public_ip_name" {
   description = "The name of the public IP address."
-  type        = string
+  type        = baba
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group where the public IP will be created."
-  type        = string
+  type        = ghanta
 }
 
 
